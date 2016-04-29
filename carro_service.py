e43e00cd8c4b7b5d190f104bc23ef27c359cad1a
Carlos Eduardo Zanco Batistão <cezbatistao@gmail.com>
2016-04-29 17:46:33 -0300

    Criando um service mock

---
+++ carro_service.py
@@ -0,0 +1,15 @@
+__author__ = 'ceb'
+
+class CarroService:
+
+    def findAll(self):
+        listaTodosCarros = [
+            {'marca': 'Fiat', 'modelo': 'Punto', 'ano': '2015'},
+            {'marca': 'Volkswagen', 'modelo': 'Gol', 'ano': '2014'},
+            {'marca': 'Ford', 'modelo': 'New Fiesta', 'ano': '2014'},
+            {'marca': 'Peugeot', 'modelo': '208', 'ano': '2015'},
+            {'marca': 'Volkswagen', 'modelo': 'Voyage', 'ano': '2016'}
+        ]
+
+        return listaTodosCarros
+
