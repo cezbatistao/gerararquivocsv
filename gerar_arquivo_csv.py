17402e91f1af9cabdc00298297fe20f5a4382611
Carlos Eduardo Zanco Batistão <cezbatistao@gmail.com>
2016-04-29 17:47:13 -0300

    Criando o arquivo que contém toda a lógica para geração do arquivo CSV

---
+++ gerar_arquivo_csv.py
@@ -0,0 +1,39 @@
+__author__ = 'ceb'
+
+import csv
+import os
+from carro_service import CarroService
+
+unidade='c'
+diretorioBase=''
+
+if os.name == 'nt':
+    diretorioBase=unidade+':'
+else:
+    diretorioBase=os.getenv("HOME")
+
+carroService = CarroService()
+
+listaModelosCarros = carroService.findAll()
+
+caminhoAbsoluto = diretorioBase + os.sep + 'csv' + os.sep
+if not os.path.exists(caminhoAbsoluto):
+    os.makedirs(caminhoAbsoluto)
+
+arquivoOutput = caminhoAbsoluto + 'python_gerando_arquivo.csv'
+
+listaParaGerarCsv = []
+for dictCarro in listaModelosCarros:
+    listaInterna = []
+    listaInterna.append(dictCarro['marca'])
+    listaInterna.append(dictCarro['modelo'])
+    listaInterna.append(dictCarro['ano'])
+
+    listaParaGerarCsv.append(listaInterna)
+
+with open(arquivoOutput, 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Marca', 'Modelo', 'Ano'])
+    for linha in listaParaGerarCsv:
+        spamwriter.writerow(linha)
+
